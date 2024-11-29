b0b6ba629f1eca89ec0a70d7cea0eeff84cfee52
Gleice Avelino <gleiceavelinor@gmail.com>
2024-11-29 10:45:48 -0300

    Add files via upload

---
+++ Ada Tech Python/1.Entrada e Saída de Dados.py
@@ -0,0 +1,15 @@
+"""
+Isso é um comentário
+Setup: Instalação | Configurações Iniciais
+Entrada e Saída de Dados
+"""
+
+print("Hello Word!")
+print("Seja bem-vindo ao curso de Pyhton do Coding Tank!")
+input("Qual a linguagem de progrfamação de que você está estudando?")
+
+
+print(42/2)
+print(42//2)
+print(42)
+
---
+++ Ada Tech Python/2.Variáveis.py
@@ -0,0 +1,18 @@
+# > PRIMEIRO CÓDIGO
+
+# 1. Variáveis
+
+idade = 26 # criando uma variável
+print(idade)
+
+nome = "Gleice Avelino"
+idade = 26
+altura = 1.77
+estudando =True
+
+print(type(nome))
+print(type(idade))
+print(type(altura))
+print(type(estudando))
+linguagem = input('Qual é a linguagem que você está estudando?')
+print(linguagem)
